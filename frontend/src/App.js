ff76458e872769acc53d6a6e38dd0cd551694985
VPT <vptamilseven@gmail.com>
2023-12-23 23:28:53 +0530

    header_with_Amazona

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div>
+      <header >
+        <a href="/">Amazona</a>
+      </header>
+      <main>
+        List Products
+      </main>
+    </div>
+  );
+}
+
+export default App;
