c6d7508310b937a63f44d9d8f0d14604fb787b2d
VPT <vptamilseven@gmail.com>
2024-01-14 23:21:50 +0530

    12.Implement_Add_To_Cart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,41 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import HomeScreen from './screen/HomeScreen';
 import ProductScreen from './screen/ProductScreen';
 import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
 import {LinkContainer} from 'react-router-bootstrap';
+import Badge from 'react-bootstrap/esm/Badge';
+import { useContext } from 'react';
+import { Store } from './Store';
 
 function App() {
+  const { state } = useContext(Store);
+  const { cart } = state;
   return (
     <BrowserRouter>
-      <div className='d-flex flex-column site-container'>
+      <div className="d-flex flex-column site-container">
         <header>
           <Navbar bg="dark" variant="dark">
             <Container>
               <LinkContainer to="/">
                 <Navbar.Brand>Amazona</Navbar.Brand>
               </LinkContainer>
+              <Nav className="me-auto">
+                <Link to="/cart" className="nav-link">
+                  Cart{cart.cartItems.length > 0 && (
+                    <Badge pill bg='danger'>
+                      {cart.cartItems.length}
+                    </Badge>
+                  )}
+                </Link>
+              </Nav>
             </Container>
           </Navbar>
         </header>
 
         <main>
-          <Container className='mt-3'>
+          <Container className="mt-3">
             <Routes>
               <Route path="/product/:slug" element={<ProductScreen />} />
               <Route path="/" element={<HomeScreen />} />
@@ -28,7 +43,7 @@ function App() {
           </Container>
         </main>
         <footer>
-          <div className='text-center'>All rights reserved</div>
+          <div className="text-center">All rights reserved</div>
         </footer>
       </div>
     </BrowserRouter>
--- frontend/src/screen/ProductScreen.js
+++ frontend/src/screen/ProductScreen.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-import React, { useEffect, useReducer } from 'react';
-import Col from 'react-bootstrap/esm/Col';
+import React, { useContext, useEffect, useReducer } from 'react';
+import Col, { useCol } from 'react-bootstrap/esm/Col';
 import Row from 'react-bootstrap/esm/Row';
 import { useParams } from 'react-router-dom';
 import Rating from '../components/Rating';
@@ -12,6 +12,7 @@ import {Helmet} from 'react-helmet-async';
 import LoadingBox from '../components/LoadingBox';
 import MessageBox from '../components/MessageBox';
 import { getError } from '../utill';
+import { Store } from '../Store';
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -50,6 +51,14 @@ const ProductScreen = () => {
     };
     fetchData();
   }, [slug]);
+
+  const {state,dispatch:ctxDispatch} = useContext(Store);
+  const addToCartHandler = () => {
+    ctxDispatch({
+      type: 'CART_ADD_ITEM',
+        payload:{...product,quantity:1}
+    })
+  }
 
   return loading ? (
     <LoadingBox />
@@ -109,7 +118,7 @@ const ProductScreen = () => {
                 {product.countInStock > 0 && (
                   <ListGroup.Item>
                     <div className="d-grid">
-                      <Button variant="primary">Add to Cart</Button>
+                      <Button onClick={addToCartHandler} variant="primary">Add to Cart</Button>
                     </div>
                   </ListGroup.Item>
                 )}
