7235d43819874157b96b6417d4f2e3ef7c30cbf9
VPT <vptamilseven@gmail.com>
2024-02-04 14:51:39 +0530

    13. Complete_Add_To_Cart

--- backend/Server.js
+++ backend/Server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import data from './data.js';
+
 const app = express();
 
 app.get('/api/products', (req, res) => { 
@@ -15,6 +16,14 @@ app.get('/api/products/slug/:slug', (req, res) => {
         res.status(404).send({ message: 'Product not found' });
     }
   
+});
+app.get('/api/products/:id', (req, res) => {
+  const product = data.products.find((x) => x._id === req.params.id);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: 'Product not found' });
+  }
 });
 
 const port = process.env.PORT || 5000;
